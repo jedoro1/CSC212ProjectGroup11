d89ef3b5258be59eaa45e0e86dd9a412deab3108
jackie <jedoro@uri.edu>
2023-11-28 23:15:24 -0500

    This is more or less everything we need for the project

--- Btree.cpp
+++ Btree.cpp
@@ -3,8 +3,73 @@
 //
 #include "Btree.h"
 #include <iostream>
-Node::Node() {
 
 
+//constructor that takes in minimum number of keys and isLeaf
+Node::Node(int min_key, bool leaf){
+    //we can start with min key 2 for simplicity
+    //initializing minimum number of keys and if it is a leaf
+    this->min=min_key;
+    this->isLeaf=false;
+    //resizing keys and child vectors
+    this->keys.resize(2*t-1);
+    this->child.resize(2*t);
+    //no keys have been inserted so num keys is 0
+    this->num_keys=0;
+}
+
+
+///
+////PRIVATE SECTION
+///
+
+//traverse all nodes in a subtree rooted with node
+void traverse(BTreeNode* node){
+    // Implement your insertion logic here
+    //I would leave this until last since we may or
+    //may not need it for the ouput
+}
+//insert helper
+void insert_non_full(Btree* root, int data){
+    // Implement your insertion logic here
+}
+//insert helper
+void split_child(Btree* root, int data){
+    // Implement your insertion logic here
+}
+//main insert
+Node* Btree::insert(int data, Node* root) {
+    // Implement your insertion logic here
+    return nullptr;
+}
+//search for data in subtree of node given
+int Btree::search(int data, Node* root) {
+    // Implement your search logic here
+    return nullptr;
+}
+
+////
+////PUBLIC SECTION
+///
+
+
+//Btree constructor
+Btree::Btree(int degree) {
+    //we set the minimum degree and root as nullptr as there is
+    //nothing in the Btree. It will be filled in the insert.
+    this->min=degree;
+    this->root=nullptr;
+}
+
+//Public insert called by user
+void Btree::insert(int data) {
+    //setting current root to inserted data
+    this->root = insert(data, this->root);
+}
+
+//Public search called by user
+void Btree::search(int data) {
+    //search returns count of data in Btree
+   this->repeated_num=search(int data, this->root);
 }
 
--- Btree.h
+++ Btree.h
@@ -1,26 +1,64 @@
 //
 // Created by Jacqueline Edoro on 11/24/23.
+
+//**B-Tree**
+
+//- ensure at least `insert` & `search` methods are correct
+//- `search` must also return/print the count
+//
+//- generate a DOT file for visualization
+//
+//        **_Store the words of an input text file_**
+//
+//- for the word ($node$) in the tree, a count with the number of repeats must also be stored
+
 #pragma once
 #include <vector>
+
+
 Class Node{
     private:
-        std::vector<KeyType> keys;
-        // Keys stored in the node
-        std::vector<ValueType> values;
-        // Corresponding values for the keys (if the B-tree is used as a map)
-        std::vector<Node*> children;
-        // Pointers to child nodes
+        // Keys stored in a vector as integers
+        std::vector<int> keys;
+        //minimum number of keys each node requires
+        int min;
+        //stores vector of child pointers
+        std::vector<Node*> child;
+        //current number of keys
+        int num_keys;
+        //checks if the node is a leaf, True if leaf, false if not
         bool isLeaf;
-        // Indicates whether the node is a leaf or internal node
+
         friend class Btree;
     public:
-        Node();
+
+        //constructor that takes in minimum number of keys and isLeaf
+        Node(int min_key, bool leaf);
 
 };
 
 Class Btree{
 
     private:
+        Node* root;
+        int min;
+        //Main insert, directs to methods like split_child/insert_non_full
+        Node* insert(int data, Node* root);
 
+        //searches the tree finding repeats, returns count(see above guidelines)
+        Node* search(int data, Node* root);
+        //preorder, post order, in order (probably needed so we generate dot file)
+        void traverse(BTreeNode* node);
+        //inserts into a non full node
+        void insert_non_full(Btree* root, int data);
+        //splits nodes if too big
+        void split_child(Btree* root, int data);
     public:
+        //constructor of the B tree class takes in degree
+        Btree(int degree);
+
+        void insert(int data);
+
+        void search(int data);
+        void traverse();
 };
