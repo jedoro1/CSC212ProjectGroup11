d8bd495383f8e9b92d8a2022204822eb52b06b58
OldManJensen <98491702+OldManJensen@users.noreply.github.com>
2023-12-03 13:44:04 -0500

    Update main.cpp
    Main cpp file for educational program

--- main.cpp
+++ main.cpp
@@ -6,44 +6,8 @@
 
 int main(int argc, char* argv[]) {
     Btree btree(3);
-    LLRBTree llrbtree;
-    BSTree bstree;
 
-    std::cout << "V To show the B-Tree class works V" << std::endl;
-
-    std::cout << "Inserting 5 numbers into B-Tree..." << std::endl;
-    btree.insert(5);
-    btree.insert(9);
-    btree.insert(12);
-    btree.insert(15);
-    btree.insert(4);
-
-    std::cout<<std::endl;
-    std::cout<<"Showcasing traverse method"<<std::endl;
-    btree.traverse();
-
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-
-    std::cout<<"Inserting 7 into tree..."<<std::endl;
-
-    btree.insert(7);
-
-    std::cout<<std::endl;
-    std::cout<<"Showing new btree..."<<std::endl;
-    btree.traverse();
-
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-
-    std::cout<<"Searching for 7 in btree..."<<std::endl;
-    btree.search(7);
-
-    std::cout<<std::endl;
-    std::cout<<"Searching for 8 in btree.."<<std::endl;
-    btree.search(8);
-
-
+    int random_num = std::stoi(argv[1]);
 
     //Code from <random> library to get random numbers from 1 to whatever the user inputs
     // Seed for the random number generator
@@ -51,71 +15,78 @@ int main(int argc, char* argv[]) {
     // Mersenne Twister random number engine
     std::mt19937 gen(rd());
     // Define the range for random numbers (1 to userAmount)
-    std::uniform_int_distribution<int> distribution(1, 10000);
+    std::uniform_int_distribution<int> distribution(1, random_num);
 
-    int randomNum;
-    for(int i = 0; i < 100; i++){
-        randomNum = distribution(gen);
-        btree.insert(randomNum);
+    std::cout << "Inserting " << random_num <<" random numbers into the B-Tree..." << std::endl;
+    int number;
+    for(int i = 1; i <= random_num; i++){
+        number = distribution(gen);
+        btree.insert(number);
         //llrbtree.insert(randomNum);
         //bstree.insert(randomNum);
 
     }
 
-    //CODE FOR B-TREE TIME COMPLEXITY
-
-    float BTinsertT = time_func_Btree(btree, "insert", 101);
-    float BTsearchT = time_func_Btree(btree, "search", 55);
-    float BTtraverseT = time_func_Btree(btree, "traverse", 0);
-
+    std::cout << "V YOUR B-TREE V" << std::endl;
+    btree.traverse();
     std::cout << std::endl;
 
-    std::cout << std::endl;
-    std::cout << "** BTree TIME COMPLEXITY **" << '\n';
-    std::cout << '\n';
-    std::cout << "Insertion time for B-Tree --> " << BTinsertT << " seconds" << std::endl;
-    std::cout << "Search time for B-Tree --> " << BTsearchT << " seconds" << std::endl;
-    std::cout << "Traverse time for B-Tree --> " << BTtraverseT << " seconds" << std::endl;
-    std::cout << std::endl;
-    
-    //CODE FOR BST TIME COMPLEXITY
-   
-    std::cout << "** BST TIME COMPLEXITY **" << '\n';
-    std::cout << '\n';
-   
-   float BSTinsertT = time_func_BST(bstree, "insert", 101, bstree.root);
-   float BSTsearchT = time_func_BST(bstree, "search", 55, bstree.root);
-    float BSTinorderT = time_func_BST(bstree, "inorder", 0, bstree.root);
-   float BSTpreorderT = time_func_BST(bstree, "preorder", 0, bstree.root);
-   float BSTpostorderT = time_func_BST(bstree, "postorder", 0, bstree.root);
+    ////USER INPUT SECTION
+    std::string method = "";
+    while(method != "done") {
 
-    std::cout << "Insertion time for BSTree --> " << BSTinsertT << " seconds" << std::endl;
-    std::cout << "Search time for BSTree --> " << BSTsearchT << " seconds" << std::endl;
-   std::cout << "Inorder time for BSTree --> " << BSTinorderT << " seconds" << std::endl;
-   std::cout << "Preorder time for BSTree --> " << BSTpreorderT << " seconds" << std::endl;
-   std::cout << "Postorder time for BSTree --> " << BSTpostorderT << " seconds" << std::endl;
+        std::cout << "Which method do you want to use?" << std::endl;
+        std::cout<<std::endl;
+        std::cout << "Your options are:" << std::endl;
+        std::cout << "insert (Inputs a number into the B-Tree)" << std::endl;
+        std::cout << "search (searches for a number into the B-Tree)" << std::endl;
+        std::cout << "traverse (Inputs a number into the B-Tree)" << std::endl;
+        std::cout << "done (Ends the program)" << std::endl;
+        std::cin >> method;
 
-   std::cout << std::endl;
+        ////USER INPUTS INSERT
+        if(method == "insert"){
+            int key;
+            std::cout << "What number do you want to insert into the B-Tree?" << std::endl;
 
+            std::cout << std::endl;
 
-    //CODE FOR LLBR TIME COMPLEXITY
+            std::cin>>key;
+            std::cout << "Inserted number into tree below..."<<std::endl;
+            float BTinsertT = time_func_Btree(btree, "insert", key);
+            float BTtraverseT = time_func_Btree(btree, "traverse", 0);
+            std::cout << std::endl;
+            std::cout << "Insertion time for B-Tree --> " << BTinsertT << " seconds" << std::endl;
+            std::cout << "Traverse time for B-Tree --> " << BTtraverseT << " seconds" << std::endl;
+        }
 
-    std::cout << "** LLRBTree TIME COMPLEXITY **" << '\n';
-    std::cout << '\n';
+        ////USER INPUTS SEARCH
+        else if(method == "search"){
+            int key;
+            std::cout << "What number do you want to search for in the B-Tree?" << std::endl;
+            std::cin>>key;
+            float BTsearchT = time_func_Btree(btree, "search", key);
+            std::cout<<std::endl;
+            std::cout << "Search time for B-Tree --> " << BTsearchT << " seconds" << std::endl;
+        }
 
-   float LLRBinsertT = time_func_LLRB(llrbtree, "insert", 101);
-   float LLRBsearchT = time_func_LLRB(llrbtree, "search", 55);
-   float LLRBinorderT = time_func_LLRB(llrbtree, "inorder", 0);
-   float LLRBpreorderT = time_func_LLRB(llrbtree, "preorder", 0);
-   float LLRBpostorderT = time_func_LLRB(llrbtree, "postorder", 0);
+        ////USER INPUTS TRAVERSE
+        else if(method == "traverse"){
+            float BTtraverseT = time_func_Btree(btree, "traverse", 0);
+            std::cout << std::endl;
+            std::cout << "Traverse time for B-Tree --> " << BTtraverseT << " seconds" << std::endl;
 
-   std::cout << "Insertion time for BSTree --> " << LLRBinsertT << " seconds" << std::endl;
-   std::cout << "Search time for BSTree --> " << LLRBsearchT << " seconds" << std::endl;
-   std::cout << "Inorder time for BSTree --> " << LLRBinorderT << " seconds" << std::endl;
-   std::cout << "Preorder time for BSTree --> " << LLRBpreorderT << " seconds" << std::endl;
-   std::cout << "Postorder time for BSTree --> " << LLRBpostorderT  << " seconds" << std::endl;
+        }
 
-   std::cout << std::endl;
+        ////USER INPUTS DONE
+        else if(method == "done"){
+            std::cout << "Thanks for using our program to learn more about B-Trees!" << std::endl;
+            return 0;
+        }
+        else{
+            std::cout << "Please try again... type the method you want exactly as it's shown in the menu." << std::endl;
+        }
+    }
 
 
     return 0;
