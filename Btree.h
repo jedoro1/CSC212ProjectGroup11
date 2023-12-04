532d821c0c469cb1601719d5f35bb4dacf712926
OldManJensen <98491702+OldManJensen@users.noreply.github.com>
2023-12-03 20:08:42 -0500

    Update Btree.h

--- Btree.h
+++ Btree.h
@@ -1,76 +1,82 @@
-//
-// Created by Jacqueline Edoro on 11/24/23.
-
-//**B-Tree**
-
-//- ensure at least `insert` & `search` methods are correct
-//- `search` must also return/print the count
-//
-//- generate a DOT file for visualization
-//
-//        **_Store the words of an input text file_**
-//
-//- for the word ($node$) in the tree, a count with the number of repeats must also be stored
-
 #pragma once
 #include <vector>
+#include <sstream>
+#include <fstream>
 
 
 class Node{
 private:
-        // Keys stored in a vector as integers
-        int* keys;
+    // Keys stored in a vector as integers
+    int* keys;
 
-        //minimum number of keys each node requires
-        int min_key;
+    std::string* s_keys;
 
-        //stores vector of child pointers
-        std::vector<Node*> children;
+    //minimum number of keys each node requires
+    int min_key;
 
-        //current number of keys
-        int num_keys;
+    //stores vector of child pointers
+    std::vector<Node*> children;
 
-        //checks if the node is a leaf, True if leaf, false if not
-        bool isLeaf;
+    //current number of keys
+    int num_keys;
 
-        //splits nodes if too big
-        void split_child(int index, Node* child);
+    //checks if the node is a leaf, True if leaf, false if not
+    bool isLeaf;
 
-        //preorder, post order, in order (probably needed so we generate dot file)
-        void traverse(int key, int repeated_nums);
+    //splits nodes if too big
+    void split_child(int index, Node* child);
 
-        //inserts into a non full node
-        void insert_non_full(int key);
+    void split_child_string(int index, Node*child);
+
+    //preorder, post order, in order (probably needed so we generate dot file)
+    int traverse(int key, int repeated_nums);
+
+    //Same code but for strings
+    int traverse(std::string key, int repeated_nums);
+
+    //inserts into a non full node
+    void insert_non_full(int key);
+
+    void insert_non_full(std::string key);
 
 public:
-        //constructor that takes in minimum number of keys and isLeaf
-        Node(int min_key, bool isLeaf);
+    //constructor that takes in minimum number of keys and isLeaf
+    Node(int min_key, bool isLeaf);
 
-        //searches the tree finding repeats, returns count(see above guidelines)
-        Node* search(int key);
+    //searches the tree finding repeats, returns count(see above guidelines)
+    Node* search(int key);
 
-        friend class Btree;
+    Node* search(std::string key);
+
+    friend class Btree;
 
 };
 
 class Btree{
 
 private:
-        Node* root;
+    Node* root;
 
-        //Minimum degree of Btree
-        int min_key;
+    //Minimum degree of Btree
+    int min_key;
+
 
 public:
-        //constructor of the B tree class takes in degree
-        Btree(int min_key);
+    //constructor of the B tree class takes in degree
+    Btree(int min_key);
 
-        //Public method to call private insert method
-        void insert(int key);
+    //Public method to call private insert method
+    void insert(int key);
 
-        //Public method to call private search method
-        Node* search(int key);
+    void insert(std::string key);
 
-        //Public method to call private traverse method
-        void traverse(int key);
-};
+    //Public method to call private search method
+    Node* search(int key);
+
+    Node* search(std::string key);
+
+    //Public method to call private traverse method
+    void traverse(int key);
+
+    void traverse(std::string key);
+
